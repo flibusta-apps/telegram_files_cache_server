368aa85817cd19a0653d3ed86fce579dbc3b8659
Bulat Kurbanov <kurbanovbul@gmail.com>
2022-04-11 20:57:20 +0300

    Set ORJSONResponse as default

--- src/core/app.py
+++ src/core/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
 from prometheus_fastapi_instrumentator import Instrumentator
 
@@ -8,7 +9,7 @@ from core.db import database
 
 
 def start_app() -> FastAPI:
-    app = FastAPI()
+    app = FastAPI(default_response_class=ORJSONResponse)
 
     app.state.database = database
 
