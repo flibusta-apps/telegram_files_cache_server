a9c57e3c7c32471c3caf6a40ea579e07c535b773
Bulat Kurbanov <kurbanovbul@gmail.com>
2022-08-10 13:11:15 +0300

    Add defer for check_books_page task

--- src/app/services/cache_updater.py
+++ src/app/services/cache_updater.py
@@ -55,8 +55,8 @@ async def check_books(ctx: dict, *args, **kwargs) -> None:  # NOSONAR
     except httpx.ConnectError:
         raise Retry(defer=15)
 
-    for page_number in range(books_page.total_pages, 0, -1):
-        await arq_pool.enqueue_job("check_books_page", page_number)
+    for i, page_number in enumerate(range(books_page.total_pages, 0, -1)):
+        await arq_pool.enqueue_job("check_books_page", page_number, _defer_by=2 * i)
 
 
 async def cache_file(book: Book, file_type: str) -> Optional[CachedFile]:
