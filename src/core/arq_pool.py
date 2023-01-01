3ed43b002eeda5a170e0ac672e09a6317ba82762
Bulat Kurbanov <kurbanovbul@gmail.com>
2023-01-01 20:53:52 +0100

    Fix

--- src/core/arq_pool.py
+++ src/core/arq_pool.py
@@ -1,4 +1,5 @@
 from arq.connections import create_pool, RedisSettings, ArqRedis
+import msgpack
 
 from core.config import env_config
 
@@ -12,4 +13,8 @@ def get_redis_settings() -> RedisSettings:
 
 
 async def get_arq_pool() -> ArqRedis:
-    return await create_pool(get_redis_settings())
+    return await create_pool(
+        get_redis_settings(),
+        job_serializer=msgpack.packb,  # type: ignore
+        job_deserializer=lambda b: msgpack.unpackb(b, raw=False),  # noqa: E731
+    )
