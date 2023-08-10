7bb39281d2821e577d60a73492ca40e4f521d493
Bulat Kurbanov <kurbanovbul@gmail.com>
2023-08-10 21:32:14 +0200

    Fix

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ pub mod prisma;
 pub mod views;
 pub mod services;
 
-use std::net::SocketAddr;
+use std::{net::SocketAddr, str::FromStr};
+use sentry::{ClientOptions, types::Dsn, integrations::debug_images::DebugImagesIntegration};
 use tracing::info;
 
 use crate::views::get_router;
@@ -12,6 +13,15 @@ use crate::views::get_router;
 
 #[tokio::main]
 async fn main() {
+    let options = ClientOptions {
+        dsn: Some(Dsn::from_str(&config::CONFIG.sentry_dsn).unwrap()),
+        default_integrations: false,
+        ..Default::default()
+    }
+    .add_integration(DebugImagesIntegration::new());
+
+    let _guard = sentry::init(options);
+
     tracing_subscriber::fmt()
         .with_target(false)
         .compact()
