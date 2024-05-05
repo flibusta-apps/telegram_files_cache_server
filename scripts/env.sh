08f52381d29ee8536b2714b2b255e38921538c66
Bulat Kurbanov <kurbanovbul@gmail.com>
2024-05-05 15:03:47 +0200

    Fix

--- scripts/env.sh
+++ scripts/env.sh
@@ -4,4 +4,4 @@ response=`curl -X 'GET' "https://$VAULT_HOST/v1/$VAULT_SECRET_PATH" -s \
   -H 'accept: application/json' \
   -H "X-Vault-Token: $VAULT_TOKEN"`
 
-echo "$(echo "$response" | jq -r '.data.data | to_entries | map("\(.key)=\(.value)") | .[]')"
+echo "$(echo "$response" | jq -r '.data.data | to_entries | map("\(.key)='\''\(.value)'\''") | .[]')"
